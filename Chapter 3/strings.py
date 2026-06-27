246a28ee076dda6a8b48949e544ccfd3676e41af
HafizSharimSohail-py <hafizsharims@gmail.com>
2026-06-27 13:32:04 +0500

    Updated Python Chapters

---
+++ Chapter 3/strings.py
@@ -0,0 +1,14 @@
+# doublequote = "Sharim" # Double Quote String
+# singlequote = 'Sharim' # Single Quote String
+# triplequote = '''Sharim''' # Triple quote String
+
+# ============String Slicing==================
+
+# name = "Sharim"
+# print(name[0:3])
+# print(name[2:5])
+
+# ===============Negative Indices==================
+# name = "Sohail"
+# print(name[-6:-3])
+
