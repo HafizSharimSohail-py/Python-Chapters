e91fad5199d1d94160203035809dbb9e550e08ac
HafizSharimSohail-py <hafizsharims@gmail.com>
2026-07-05 19:25:44 +0500

    Updated code

--- Chapter 3/strings.py
+++ Chapter 3/strings.py
@@ -8,7 +8,54 @@
 # print(name[0:3])
 # print(name[2:5])
 
+# name = "Sohail"
+# print(name[2:5])
+
 # ===============Negative Indices==================
 # name = "Sohail"
-# print(name[-6:-3])
+# print(name[-6:-4])
 
+# name = "Sohail"
+# print(name[-4:-2])
+
+# ==================Skip===========================
+# name = "Sharim"
+# print(name[1:5:3])
+
+# name = "Sohail"
+# print(name[1:6:3])
+
+# ====================Strings Functions=========================
+# Length of string
+# name = 'sharim'
+# print(len(name))
+
+# name = "Sohail"
+# print(len(name))
+
+# end functions
+# name = "Sharim"
+# print(name.endswith("rim"))
+
+name = "Sohail"
+print
+
+# string.count
+# name = "Sharim sohail"
+# count = name.count("a")
+# print(count)
+
+# first character of string is capalized
+# name = "sharim"
+# capalized = name.capitalize()
+# print(capalized)
+
+# string.find
+# name = "Nabeel"
+# index = name.find("l")
+# print(index)
+
+# replace
+# name = "Sharim"
+# replace_string = name.replace("S", "M")
+# print(replace_string)
